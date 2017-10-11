d2867d518d3b500976ab4fc59539225fc613c7ed
Jordan <j.drespincho@yahoo.com>
2017-10-11 18:00:04 +0200

    Tentative Shuffle_Manager

---
+++ Assets/Scripts/IG/Shuffle_Manager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shuffle_Manager : MonoBehaviour {
+
+	public GameObject[] slots;
+
+	public List<GameObject> tmpSlots = new List<GameObject>();
+
+	// public GameObject slot2;
+	// public GameObject slot3;
+	// public GameObject slot4;
+	// public GameObject slot1;
+	// public GameObject slot5;
+	// public GameObject slot6;
+	// public GameObject slot7;
+	// public GameObject slot8;
+	// public GameObject slot9;
+
+	// public GameObject[] tmpSlots;
+	// Use this for initialization
+	void Start () {
+		// tmpSlots = new GameObject[8];
+		ShuffleManager();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ShuffleManager(){
+		// for (i = 0; i < 9; i++){
+		// 	tmpSlots.Add(slots[0]);
+		// }
+		
+	}
+}
