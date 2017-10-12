ea6929c6040d502deebf17043a715b76c7bc5abf
Killian Beguerie <killian.beguerie@e-artsup.net>
2017-10-12 10:18:06 +0200

    Re game manager

--- Assets/Scripts/IG/Game_Manager.cs
+++ Assets/Scripts/IG/Game_Manager.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game_Manager : MonoBehaviour {
 
+	private bool isSelectionActive = false;
+	private string actualTag ;
+	private int actualMatchs = 0 ;
+
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+	public void OnClickRune (Button button)
+	{
+		if (isSelectionActive == false)
+		{
+			actualTag = button.tag;
+			actualMatchs += 1;
+			isSelectionActive = true;
+			button.interactable = false;
+		}
+		if (isSelectionActive == true)
+		Debug.Log(actualTag);
 	}
 }
