f789899e95be5492019858efa961835af3e7c845
Jordan <j.drespincho@yahoo.com>
2017-10-13 09:42:03 +0200

    Update quasi fonctionnelle du Time Manager

--- Assets/Scripts/UI/Timer_Manager.cs
+++ Assets/Scripts/UI/Timer_Manager.cs
@@ -1,30 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer_Manager : MonoBehaviour {
 
 	public float timerInSeconds;
 	public float tmpTimer;
-	public GameObject spawnSystem;
+	// public GameObject spawnSystem;
+	public Text timer;
 	// private Spawn_Manager spawnManager = spawnSystem.GetComponent<Spawn_Manager>();
 
 	// Use this for initialization
 	void Start () {
-		Spawn_Manager spawnManager = spawnSystem.GetComponent<Spawn_Manager>();
+		// Spawn_Manager spawnManager = spawnSystem.GetComponent<Spawn_Manager>();
 		tmpTimer = timerInSeconds;
+		// timer = gameObject.GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		tmpTimer -= Time.deltaTime;
-		if (tmpTimer < 0){
+		timer.text = tmpTimer.ToString("F2")+" s";
+		if (tmpTimer <= 0.0f){
 			ResetTimer();
 		}
 	}
 
 	public void ResetTimer(){
-		// spawnManager.SpawnForm();
+		// spawnSystem.GetComponent<Spawn_Manager>().SpawnForm();;
+		Debug.Log("Reset");
 		tmpTimer = timerInSeconds;
 	}
 }
