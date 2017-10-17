c7cc1223fe1f4318cd1f1725c93c59e409e6a6ae
Jordan <j.drespincho@yahoo.com>
2017-10-17 15:30:51 +0200

    Shield_Manager à intégrer

---
+++ Assets/Scripts/IG/Shield_Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shield_Manager : MonoBehaviour {
+
+	public Image shield;
+	public float shieldRefill;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shield.fillAmount < 1.0f){
+			if (Input.GetKeyDown(KeyCode.E)){
+				Debug.Log("Refill");
+				RefillShield();
+			}
+			
+		}
+	}
+
+	public void RefillShield(){
+		if((shield.fillAmount + shieldRefill) <= 1.0f){
+			shield.fillAmount += shieldRefill;
+		} else if ((shield.fillAmount + shieldRefill) > 1.0f){
+			shield.fillAmount = 1.0f;
+		}
+	}
+}
